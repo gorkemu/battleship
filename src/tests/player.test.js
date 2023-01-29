fdfebe287d27b2b94a3df75dc86afed4ad5e2d0c
gorkemu <gorkemunal88@gmail.com>
2023-01-29 23:53:58 +0300

    organize tests

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -2,20 +2,15 @@ import { Player } from "../player";
 import { AI } from "../player";
 import { Gameboard } from "../gameboard";
 import { Ship } from "../ship";
-import { Game } from "../game";
 
-describe("attack", () => {
+describe("player attack", () => {
   let player;
-  let ai;
-  let playerBoard;
   let enemyBoard;
   let enemyShip;
   beforeEach(() => {
-    player = Player("Test Player");
-    ai = AI("AI");
-    playerBoard = Gameboard();
+    player = Player();
     enemyBoard = Gameboard();
-    enemyShip = Ship(4);
+    enemyShip = Ship(3);
     enemyBoard.placeShip(enemyShip, 6, "x");
   });
 
@@ -24,15 +19,20 @@ describe("attack", () => {
     expect(enemyBoard.board[4].isShot).toBe(true);
   });
 
-  test("a random cell should be shot with random attack", () => {
-    ai.randomAttack(playerBoard);
-    const shotSpots = playerBoard.board.filter((spot) => spot.isShot === true);
-    expect(shotSpots.length).toBe(1);
+  test("if it's a hit, increment ship's hits after attack", () => {
+    player.attack(enemyBoard, 6);
+    expect(enemyBoard.board[6].ship.hits).toBe(1);
   });
 
-  test("enemy ship should be hit with attack on it", () => {
+  test("positions should be added to player's previousAttacks", () => {
+    player.attack(enemyBoard, 4);
+    player.attack(enemyBoard, 5);
+    expect(player.previousAttacks).toEqual([4, 5]);
+  });
+
+  test("the ship on the attacked cell should be equal to the actual ship ", () => {
     player.attack(enemyBoard, 6);
-    expect(enemyShip.hits).toBe(1);
+    expect(enemyBoard.board[6].ship).toEqual(enemyShip);
   });
 
   test("sink ship after 4 attacks", () => {
@@ -49,5 +49,32 @@ describe("attack", () => {
     player.attack(enemyBoard, 8);
     player.attack(enemyBoard, 9);
     expect(enemyBoard.isAllSunk()).toBe(true);
+  });
+});
+
+describe("AI attack", () => {
+  let ai;
+  let playerBoard;
+  let playerShip;
+  beforeEach(() => {
+    ai = AI();
+    playerBoard = Gameboard();
+    playerShip = Ship(10);
+    playerBoard.placeShip(playerShip, 0, "x");
+    playerBoard.placeShip(playerShip, 10, "x");
+    playerBoard.placeShip(playerShip, 20, "x");
+    playerBoard.placeShip(playerShip, 30, "x");
+    playerBoard.placeShip(playerShip, 40, "x");
+    playerBoard.placeShip(playerShip, 50, "x");
+    playerBoard.placeShip(playerShip, 60, "x");
+    playerBoard.placeShip(playerShip, 70, "x");
+    playerBoard.placeShip(playerShip, 80, "x");
+    playerBoard.placeShip(playerShip, 90, "x");
+  });
+
+  test("a cell should be shot after attack", () => {
+    ai.attack(playerBoard);
+    const shotSpots = playerBoard.board.filter((spot) => spot.isShot === true);
+    expect(shotSpots.length).toBe(1);
   });
 });
